a56399aa17d7ff82977ff18f246c271c96dad5ed
Nevena <nevena.dimitrova2021@gmail.com>
2026-01-08 18:06:06 +0200

    added main and CMakeLists

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "graphs.h"
+#include "set.h"
+
+
+int main (){
+    int adj_matrix[6][6] = {
+        {0, 1, 0, 1, 0, 1}, 
+        {1, 0, 1, 0, 0, 0}, 
+        {0, 1, 0, 0, 0, 1}, 
+        {1, 0, 0, 0, 1, 1},
+        {0, 0, 0, 1, 0, 1}, 
+        {1, 0, 1, 1, 1, 0}
+    };
+
+
+
+}
